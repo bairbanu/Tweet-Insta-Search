0cb61b7efc3031c06051dac72201089df8a0ce4e
Zubair Ahmed <zubair.ahmed@alumni.stanford.edu>
2018-04-15 09:42:36 -0700

    switch from POST endpoint to GET endpoint with query

--- server/routes/twitter.js
+++ server/routes/twitter.js
@@ -6,9 +6,8 @@ const urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 const getTweets = require('../../API/tweets')
 
-router.post('/', urlencodedParser, async (req, res) => {
-    const { searchTerm } = req.body
-    console.log('this is the req.body:::', req.body)
+router.get('/', async (req, res) => {
+    const { searchTerm } = req.query
     // const tweets = await getTweets(searchTerm)
     // console.log('these are the tweets:::', tweets)
     // res.send(tweets)
