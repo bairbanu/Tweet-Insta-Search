c6698c338c8e9cda96562fee0d4a205f8995f168
Zubair Ahmed <zubair.ahmed@alumni.stanford.edu>
2018-04-15 12:29:32 -0700

    add insta-search feature

--- frontend/components/TweetSearch/TweetSearch.js
+++ frontend/components/TweetSearch/TweetSearch.js
@@ -1,3 +1,4 @@
+import _ from 'lodash'
 import React, { Component } from 'react'
 import axios from 'axios'
 
@@ -10,20 +11,29 @@ export default class TweetSearch extends Component {
         tweetList: []
     }
 
-    onSubmit = async event => {
+    onSubmitSearch = async event => {
         event.preventDefault()
 
         const { data } = await axios.get(`http://localhost:3000/tweets?searchTerm=${this.state.value}`)
         this.setState({ tweetList: data })
     }
 
+    instaSearch = event => {
+        this.setState({ value: event.target.value })
+
+        setTimeout(async () => {
+            const { data } = await axios.get(`http://localhost:3000/tweets?searchTerm=${this.state.value}`)
+            this.setState({ tweetList: data })
+        }, 500)
+    }
+
     render() {
         return (
             <div className='app-container'>
-                <form onSubmit={ this.onSubmit }>
+                <form onSubmit={ this.onSubmitSearch }>
                     <input 
                         value={ this.state.value } 
-                        onChange={ event => this.setState({ value: event.target.value })} 
+                        onChange={ this.instaSearch } 
                         placeholder='Instantly search latest tweets...'
                     />
                     <button style={{ display: 'none' }}> For UX - just press enter </button>
