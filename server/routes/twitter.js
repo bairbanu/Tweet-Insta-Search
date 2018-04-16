f58d141e451c94e8b1575dc27bc767d9afa24944
Zubair Ahmed <zubair.ahmed@alumni.stanford.edu>
2018-04-16 10:07:03 -0700

    move API folder under server

--- server/routes/twitter.js
+++ server/routes/twitter.js
@@ -4,7 +4,7 @@ const router = express.Router()
 const bodyParser = require('body-parser')
 const urlencodedParser = bodyParser.urlencoded({ extended: false })
 
-const getTweets = require('../../API/tweets')
+const getTweets = require('../API/tweets')
 
 router.get('/', async (req, res, next) => {
     const { searchTerm } = req.query
